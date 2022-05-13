cbc70b48d599ed61fd4c7ee85236d315c1c12104
timmy <kris>
2022-05-13 15:36:34 +0200

    rolelist in userDTO

--- src/main/java/dtos/UserDTO.java
+++ src/main/java/dtos/UserDTO.java
@@ -17,12 +17,13 @@ public class UserDTO {
     public UserDTO(String userName, String password, List<Role> roleList){
         this.userName = userName;
         this.password = password;
+        this.roleList = roleList;
     }
 
     public UserDTO(User user){
         this.userName = user.getUserName();
         this.password = user.getUserPass();
-
+        this.roleList = user.getRoleList();
     }
 
     public static List<UserDTO> getDtos(List<User> user) {
@@ -36,29 +37,21 @@ public class UserDTO {
     public List<Role> getRoleList() {
         return roleList;
     }
-
     public void setRoleList(List<Role> roleList) {
         this.roleList = roleList;
     }
-
-
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
-
     public String toString(){
         return "UserDTO{" +
                 "username:" + userName +
